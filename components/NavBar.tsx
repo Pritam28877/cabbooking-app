b65c6aac6512f8dc37c36f71a7b102a1a989addb
Pritam Das <dpritam28577@gmail.com>
2023-09-26 15:32:47 +0530

    add the navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,9 +5,12 @@ const NavBar = () => {
   return (
     <div>
       <div>
-        <Image src="/Book.png" alt=""
-        width={120}
-        height={60} />
+        <Image src="/Book.png" alt="" width={120} height={60} />
+      </div>
+      <div className="flex">
+        <h1>home</h1>
+        <h1>History</h1>
+        <h1>Help</h1>
       </div>
     </div>
   );
