3fa1e7217a8f1cdb7f3f846f4e25c565d068c35b
Pritam Das <dpritam28577@gmail.com>
2023-09-26 15:30:53 +0530

    add the logo of the webapp

---
+++ components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <Image src="/Book.png" alt=""
+        width={120}
+        height={60} />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
