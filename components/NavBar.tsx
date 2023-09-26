d84787bca9edafbf453605bdb6415ffbfcf4c399
Pritam Das <dpritam28577@gmail.com>
2023-09-26 15:45:10 +0530

    add the styling to the navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import Image from "next/image";
-
+import { UserButton } from "@clerk/nextjs";
 const NavBar = () => {
   return (
-    <div>
-      <div>
+    <div
+      className="flex justify-between
+    p-3 px-10 border-b-[1px] shadow-sm"
+    >
+      <div className="flex gap-10 items-center">
         <Image src="/Book.png" alt="" width={120} height={60} />
+
+        <div className="hidden md:flex gap-6">
+          <h2
+            className="hover:bg-gray-100 p-2
+                rounded-md cursor-pointer transition-all"
+          >
+            Home
+          </h2>
+          <h2
+            className="hover:bg-gray-100 p-2
+                rounded-md cursor-pointer transition-all"
+          >
+            History
+          </h2>
+          <h2
+            className="hover:bg-gray-100 p-2
+                rounded-md cursor-pointer transition-all"
+          >
+            Help
+          </h2>
+        </div>
       </div>
-      <div className="flex">
-        <h1>home</h1>
-        <h1>History</h1>
-        <h1>Help</h1>
-      </div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
