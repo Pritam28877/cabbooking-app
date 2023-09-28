a71d801bcb3ff3a23babed82b409d501bba9418a
Pritam Das <dpritam28577@gmail.com>
2023-09-28 17:55:11 +0530

    create the map and the booking compnents

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import Image from 'next/image'
+import Booking from "@/components/Booking/Booking";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main >
-        <div className='grid grid-cols-3' >
-          <div>
-            Booking
-          </div>
-          <div className='grid-span-2' >
-            
-          </div>
-
+    <main>
+      <div className="grid grid-cols-3 md:grid-cols-3">
+        <div className="bg">
+          <Booking />
         </div>
+        <div className="col-span-2">hi</div>
+      </div>
     </main>
-  )
+  );
 }
