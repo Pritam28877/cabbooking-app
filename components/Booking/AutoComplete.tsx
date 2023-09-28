afbfe71cf0170c4ece4ea4735deff0ea5a457f06
Pritam Das <dpritam28577@gmail.com>
2023-09-28 18:13:07 +0530

    add the input fleds to the auto

--- components/Booking/AutoComplete.tsx
+++ components/Booking/AutoComplete.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const AutoComplete = () => {
   return (
-    <div>AutoComplete</div>
-  )
-}
+    <div className="mt-5">
+      <div>
+        <label htmlFor="" className="text-gray-100">
+          Where From?
+        </label>
+        <input
+          type="text"
+          placeholder="Where From?"
+          className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:ring-2 focus:ring-blue-400"
+        />
+      </div>
+      <div className="mt-3">
+        <label htmlFor="" className="text-gray-100">
+          Where To?
+        </label>
+        <input
+          type="text"
+          placeholder="Where To?"
+          className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:ring-2 focus:ring-blue-400"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default AutoComplete
+export default AutoComplete;
--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -3,7 +3,8 @@ import AutoComplete from "./AutoComplete";
 
 const Booking = () => {
   return (
-    <div>
+    <div className="p-5">
+      <h1 className="text-[20px] font-semibold">Booking</h1>
       <AutoComplete />
     </div>
   );
