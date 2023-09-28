a858565452e1619d25789db7a280300546a92854
Pritam Das <dpritam28577@gmail.com>
2023-09-28 18:21:16 +0530

    add the fix size if the window

--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import AutoComplete from "./AutoComplete";
 
 const Booking = () => {
+  const [screenHeight, setScreenHeight] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setScreenHeight(window.innerHeight * 0.72);
+    }
+  }, []);
+
   return (
     <div className="p-5">
       <h1 className="text-[20px] font-semibold">Booking</h1>
-      <AutoComplete />
+      <div className="border-[1px] p-5 rounded-md">
+        <AutoComplete />
+      </div>
     </div>
   );
 };
