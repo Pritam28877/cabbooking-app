51de3efc4b97875fce697e22a82ba3f1ff278290
Pritam Das <dpritam28577@gmail.com>
2023-09-28 17:46:59 +0530

    fix the global styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main >
-       
+        <div className='grid grid-cols-3' >
+          <div>
+            Booking
+          </div>
+          <div className='grid-span-2' >
+            
+          </div>
+
+        </div>
     </main>
   )
 }
